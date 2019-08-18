8c53dad420cd893d42e205ae79364040bf39eb31
山室貴史 <yamamurotakafumi@yamamurotakafumi.local>
2019-08-18 21:16:10 +0900

    cart途中

--- app/controllers/end_cart_items_controller.rb
+++ app/controllers/end_cart_items_controller.rb
@@ -1,16 +1,16 @@
 class EndCartItemsController < ApplicationController
   def index
-  	@carts = CartItem.where(current_end_user[:id])
+  	@carts = CartItem.where(end_user_id: current_end_user[:id])
   end
   def update
-  	cart = CartItem.where(current_end_user[:id])
+  	carts = CartItem.where(end_user_id: current_end_user[:id])
   	cart.update(cart_params)
-  	redirect_to current_cart_items
+  	redirect_to current_end_cart_items
   end
   def destroy
-  	cart = CartItem.where(current_end_user[:id])
+  	carts = CartItem.where(end_user_id: current_end_user[:id])
   	cart.destroy
-  	redirect_to current_cart_items
+  	redirect_to current_end_cart_items
   end
   def add
   	cart = CartItem.new
