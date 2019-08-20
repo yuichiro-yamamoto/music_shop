5ee29b538690a0e8089cd1f0372ebb20a3a47fdc
山室貴史 <yamamurotakafumi@yamamurotakafumi.local>
2019-08-20 16:28:06 +0900

    カート、購入途中

--- app/controllers/end_cart_items_controller.rb
+++ app/controllers/end_cart_items_controller.rb
@@ -1,24 +1,35 @@
 class EndCartItemsController < ApplicationController
   def index
   	@carts = CartItem.where(end_user_id: current_end_user[:id])
+    @carts.each do |cart|
+      item = Item.find(cart.item_id)
+      artist =Artist.find(item.artist_id)
+        purchase_quantity = cart.purchase_quantity
+        name = item.item_name
+        price = item.price
+        condition = item.sale_condition
+        image = item.item_image_id
+        artist = artists.artist_name
+    end
   end
   def update
-  	carts = CartItem.where(end_user_id: current_end_user[:id])
+  	cart = CartItem.where(end_user_id: current_end_user[:id])
   	cart.update(cart_params)
   	redirect_to current_end_cart_items
   end
+
   def destroy
-  	carts = CartItem.where(end_user_id: current_end_user[:id])
+  	cart = CartItem.where(end_user_id: current_end_user[:id])
   	cart.destroy
   	redirect_to current_end_cart_items
   end
+
   def add
   	cart = CartItem.new
     cart['item_id'] = cart_params['item_id']
     cart['user_id'] = current_end_user['id']
     cart['purchase_quantity'] = cart_params['purchase_quantity']
   	cart.save
-
   end
 
   private
--- app/helpers/end_cart_items_helper.rb
+++ app/helpers/end_cart_items_helper.rb
@@ -1,26 +1,2 @@
 module EndCartItemsHelper
-	def getModel(cart_item_model)
-		array = []
-
-		cart_item_modle.each do |cart|
-			 pq = cart['purchase_quantity']
-			 name = cart["item_id"].items["item_name"]
-			 price = cart["item_id"].items["price"]
-			 condition = cart["item_id"].items["sale_condition"]
-			 image = cart["item_id"].items["item_image_id"]
-			 artist = cart["item_id"].items[artist_id].artists["artist_name"]
-
-			 list = {}
-			 list["purchase_quantity"] = pq
-			 lits["name"] = name
-			 list["price"] = price
-			 list["condition"] = condition
-			 lits["image"] = image
-			 list["artist"] = artist
-			 array.push(list)
-		end
-
- 		#[{list},{list},{list}]
-		return  array
-	end
 end
