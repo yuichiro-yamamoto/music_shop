c95c0b3bca347f7764de0117bec8a0d506ba2b09
yuumaGH <yuuma0729b@gmail.com>
2019-08-22 00:57:39 +0900

    商品一覧、商品詳細画面の価格を税込価格に変更

--- app/controllers/end_items_controller.rb
+++ app/controllers/end_items_controller.rb
@@ -1,8 +1,9 @@
 class EndItemsController < ApplicationController
 
 	def index
+		@tax_rate = "1.08"
 		@new_items = Item.includes(:artist).page(params[:page]).per(4).order(id: "DESC")
-		@cheap_items = Item.includes(:artist).where("price <= ?", 1000).page(params[:page]).per(4).order(id: "DESC")
+		@cheap_items = Item.includes(:artist).where("price <= ?", (BigDecimal("1000") * BigDecimal(@tax_rate)).ceil).page(params[:page]).per(4).order(id: "DESC")
 		@few_items = Item.includes(:artist).where("stock <= ?", 3).page(params[:page]).per(4).order(id: "DESC")
 		@all_items = Item.includes(:artist).page(params[:page]).per(20).order(id: "DESC")
 		@ranking = Item.includes(:artist).order(sale_number: "DESC").limit(10)
@@ -15,6 +16,7 @@ class EndItemsController < ApplicationController
 	end
 
 	def show
+		@tax_rate = "1.08"
 		@item = Item.find(params[:id])
 		@review = Review.new
 		@reviews = Review.includes(:artist).page(params[:page]).per(5).order(id: "DESC")
